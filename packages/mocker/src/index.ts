06eb57eb12a76e22b1e5d2817b6539aea43c4077
刘放 <362512489@qq.com>
2019-04-29 16:53:33 +0800

    chore: init proxy server

--- packages/mocker/src/index.ts
+++ packages/mocker/src/index.ts
@@ -1,8 +1,12 @@
+import * as fs from 'fs'
+import * as  path from 'path'
+import * as http from 'http'
 import * as semver from 'semver'
-import * as express from 'express'
 import color from 'http-mockjs-util/color'
 import getConfig from './getConfig'
 import proxy from './proxy'
+import * as httpProxy from 'http-proxy'
+
 
 const pkg = require('../package.json')
 const requiredVersion = pkg.engines.node
@@ -17,16 +21,29 @@ const out = ()=>{
     process.exit(1)
   }
 
-  const app = express()
   const config = getConfig(__dirname)
 
-  app.get('/*',(req, res, next)=>{
-    const proxyURL = `${req.method} ${req.path}`
+  const serveProxy = httpProxy.createProxyServer({})
+
+  http.createServer((req,res)=>{
+    serveProxy.web(req,res,{
+      target:'http://localhost:9008'
+    })
+  }).listen(8008)
+
+  http.createServer((req,res)=>{
+    const proxyURL = `${req.method} ${req.url}`
     const proxyLists = config.routes;
-    console.log(proxyLists)
-  })
-  console.log('hehe')
-  app.listen(3000)
+    const proxyMatch = proxyLists[proxyURL]
+    //if there is a request config in the config file
+    if(proxyMatch){
+      console.log(proxyURL)
+      const curPath = path.join(process.cwd(),config.mockFileName, proxyMatch.path)
+      const responseBody = fs.readFileSync(curPath,'utf-8')
+      res.write(responseBody)
+    }
+    res.end()
+  }).listen(9008)
 }
 
 export default out
--- packages/mocker/src/proxy.ts
+++ packages/mocker/src/proxy.ts
@@ -3,7 +3,7 @@ import * as httpProxy from 'http-proxy'
 
 export default (req: import("http").IncomingMessage,res: import("http").ServerResponse,options={})=>{
     const proxy = httpProxy.createProxyServer({})
-
+    console.log()
     return proxy.web(req, res, options)
 }
 
