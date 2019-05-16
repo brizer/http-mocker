4f4ce7d790e6ad336831d8f67aa8ae6baf969c5a
刘放 <362512489@qq.com>
2019-05-16 21:19:28 +0800

    refactor: rm middle

--- packages/middleware/examples/express/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const mockjsmiddle = require('../../lib/index').default
-const mock = require('../../../mocker/lib/index').default
-const app = express()
-
-app.use(mockjsmiddle())
-// mock(app)
-app.listen(3003)
--- packages/middleware/lib/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const http_mockjs_1 = require("http-mockjs");
-const middle = (options = {}) => {
-    return (req, res, next) => {
-        http_mockjs_1.default(req.app, options);
-    };
-};
-exports.default = middle;
