fcd0e6d987bf9d05d50843f7621f601a1a4fc98f
刘放 <362512489@qq.com>
2019-06-02 09:52:30 +0800

    feat: add normalize

---
+++ packages/editor/types/styled-normalize.d.ts
@@ -0,0 +1,21 @@
+export class Normalize {
+    static styledComponentId: string;
+    constructor(props: any);
+    componentWillUnmount(): void;
+    forceUpdate(callback: any): void;
+    render(): any;
+    setState(partialState: any, callback: any): void;
+}
+export namespace Normalize {
+    const globalStyle: {
+        componentId: string;
+        createStyles: Function;
+        isStatic: boolean;
+        removeStyles: Function;
+        renderStyles: Function;
+        rules: string[];
+    };
+}
+export default _default;
+export const _default: string[];
+export const normalize: string[];
